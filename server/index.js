da563a372f327637b9bfe1c0350950ebef7c52e1
RadoslawZemlo <radoslawzemlo1@gmail.com>
2022-01-27 22:54:05 +0100

    Connect app with MongoDB Atlas

--- server/index.js
+++ server/index.js
@@ -3,18 +3,23 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const routes = require("./routes/api");
 
+require("dotenv").config();
+
+const URI = process.env.URI || "mongodb://localhost:27017/chat-app";
+const PORT = process.env.PORT || 5000;
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
 mongoose
-  .connect("mongodb://localhost:27017/chat-app")
+  .connect(URI)
   .then(() => console.log("Database connected successfully"))
   .catch(err => console.log(err));
 
 app.use("/api", routes);
 
-app.listen(5000, () => {
-  console.log("Server started on port 5000");
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
 });
